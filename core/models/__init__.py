c06983b41b5c47d0d09a2de77d695852cc9e5571
igornet0 <892130836@mail.ru>
2025-06-18 22:59:59 +0300

    Update parser

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,4 @@
-__all__ = ("Coin", "Dataset", "DatasetTimeseries")
+__all__ = ("Dataset", "DatasetTimeseries")
 
-from core.models.models import Coin
 from core.models.dataset import Dataset, DatasetTimeseries
 
